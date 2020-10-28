84864950dd85594cadcf3015637d0cc456ac3c97
Gole Oladokun <gokeladokun@gmail.com>
2020-10-28 08:43:15 +0800

    Remove build

--- gokesite/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/zs/cppweb/gokesite
-# Build directory: /mnt/d/zs/cppweb/gokesite/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
